dc78f980e303efb0180821a27d0f98e55d56ae9e
teliajm <86443998+teliajm@users.noreply.github.com>
2021-06-25 01:55:09 +0200

    Update boost.js

--- boost.js
+++ boost.js
@@ -2,9 +2,11 @@ if(!document.querySelector('#BoostTrigger') && window.location.pathname.search('
 
     const BoostTrigger = document.createElement('img');
     BoostTrigger.id = 'BoostTrigger';
-    BoostTrigger.src = 'https://300modernainternal.boost.ai/img/agent.png';
+    //BoostTrigger.src = 'https://300modernainternal.boost.ai/img/agent.png';
+    BoostTrigger.src = 'https://boost-files-general-eu-west-1-prod.s3-eu-west-1.amazonaws.com/files/MODERNAINTERNAL/QshaBkFJpUrOT6RDAGyubBlg5KFmKj_QDH-EWh_l4G6XyDhWgkfFkZGcAYip5EEBNTk_8o8FtQfuFL-FmamCwB0YOSKhm2mMto1-Zaqw6wzZqH3kwH4e7I3qY-G_Q0vDXz3EEQ==';
     BoostTrigger.classList.add('modal_trigger');
-    BoostTrigger.style = 'position: fixed;bottom: 1em;right: 2em;cursor:pointer;width: 100px;';
+    //BoostTrigger.style = 'position: fixed;bottom: 1em;right: 2em;cursor:pointer;width: 100px;';
+    BoostTrigger.style = 'position: fixed;bottom: 1em;right: 2em;cursor:pointer;width: 60px;height:60px;border-radius: 50%;';
     BoostTrigger.onload = function() {
 
         const BoostScript = document.createElement('script');
