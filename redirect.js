a7e6e652aa6eed7934152a1bca575a33f808f705
teliajm <86443998+teliajm@users.noreply.github.com>
2021-07-02 19:35:46 +0200

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,2 +1,3 @@
-alert(document.referrer);
-window.location.replace("http://127.0.0.1/redirect.html");
+if(document.referrer.search("127.0.0.1")) {
+  window.location.replace("http://127.0.0.1/redirect.html");
+} else alert("back in biz");
