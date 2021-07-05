c24cbda6f1c3872153d9be873e2cc34fd6651bb0
teliajm <86443998+teliajm@users.noreply.github.com>
2021-07-05 10:19:49 +0200

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -5,5 +5,7 @@ if(document.referrer.search("127.0.0.1")<0 && window.location.href.search("/noti
 } 
 else 
 {
+  console.log("Referrer: " + document.referrer);
+  console.log("Location: " + window.location.href);
   console.log("Running Boost-srcipt"); 
 }
