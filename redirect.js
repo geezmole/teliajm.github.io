5ba4c5373e2f4d8825d5b3c9a20c162ba5bb723b
teliajm <86443998+teliajm@users.noreply.github.com>
2021-07-05 10:12:01 +0200

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,3 +1,3 @@
-if(document.referrer.search("127.0.0.1")<0) {
+if(document.referrer.search("127.0.0.1")<0 && window.location.href.search("/notice")<0{
   window.location.replace("http://127.0.0.1/redirect.html");
 } else alert("back in biz");
