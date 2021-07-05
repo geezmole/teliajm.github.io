b3f513c0a65d4eaa7597136dfa5be801ed6755a9
teliajm <86443998+teliajm@users.noreply.github.com>
2021-07-05 10:54:30 +0200

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -7,8 +7,8 @@ if(document.referrer.search("127.0.0.1")>=0)
 } 
 else if (window.location.href.search("notice")<0)
 {
-  //window.location.replace("http://127.0.0.1/redirect.html");
-  console.log("http://127.0.0.1/redirect.html");
+  window.location.replace("http://127.0.0.1/redirect.html");
+  //console.log("http://127.0.0.1/redirect.html");
 }
 else {
   console.log("Notices in admin"); 
