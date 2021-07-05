3804e087560e113bad1667e47c72acb23a7703c3
teliajm <86443998+teliajm@users.noreply.github.com>
2021-07-05 10:42:28 +0200

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,4 +1,4 @@
-if(document.referrer.search("127.0.0.1")<0 && window.location.href.search("/notice")<0)
+if(document.referrer.search("127.0.0.1")<0 && window.location.href.search("notice")<0)
 {
   //window.location.replace("http://127.0.0.1/redirect.html");
   console.log("http://127.0.0.1/redirect.html");
