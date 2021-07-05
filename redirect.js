94733d4c2e74f51f447c432b013d273a5b586ad1
teliajm <86443998+teliajm@users.noreply.github.com>
2021-07-05 10:49:51 +0200

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,5 +1,5 @@
 //https://adfs.prd1.prdroot.net/
-if(document.referrer.search("127.0.0.1")>0 && 
+if(document.referrer.search("127.0.0.1")>=0) 
 {
   console.log("Referrer: " + document.referrer);
   console.log("Location: " + window.location.href);
