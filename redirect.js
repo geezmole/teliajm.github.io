e09ffa53e3eb6beefa09444615e69eb352a15ec2
teliajm <86443998+teliajm@users.noreply.github.com>
2021-07-05 10:48:02 +0200

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,11 +1,15 @@
-if(document.referrer.search("127.0.0.1")<0 && window.location.href.search("notice")<0)
-{
-  //window.location.replace("http://127.0.0.1/redirect.html");
-  console.log("http://127.0.0.1/redirect.html");
-} 
-else 
+//https://adfs.prd1.prdroot.net/
+if(document.referrer.search("127.0.0.1")>0 && 
 {
   console.log("Referrer: " + document.referrer);
   console.log("Location: " + window.location.href);
   console.log("Running Boost-srcipt"); 
+} 
+else if (window.location.href.search("notice")<0)
+{
+  //window.location.replace("http://127.0.0.1/redirect.html");
+  console.log("http://127.0.0.1/redirect.html");
+}
+else {
+  console.log("Notices in admin"); 
 }
