76066b5d19194fc33bdf9462cb185232490d49af
teliajm <86443998+teliajm@users.noreply.github.com>
2021-07-07 18:09:15 +0200

    Update tools.js

--- tools.js
+++ tools.js
@@ -12,5 +12,7 @@ function openTab(evt, tabName) {
   }
 
   document.getElementById(tabName).style.display = 'block';
-  evt.className += ' active';
+  //evt.className += ' active';
+  alert(evt.className);
+  evt.setAttribute("class", "tablinks active");
 }
